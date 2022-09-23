11c9ef5d26d6bda74ea9f55be4b192859c04068a
vishal5394 <vishaljadyal@gmail.com>
2022-09-23 20:34:38 +0530

    Added: Address book app

---
+++ js/site_properties.js
@@ -0,0 +1,6 @@
+const site_properties = {
+    use_local_storage: "false",
+    home_page: "../pages/AddressBookHome.html",
+    add_contacts_page: "../pages/AddressBookForm.html",
+    server_url: "http://127.0.0.1:3000/Contacts"
+  };
